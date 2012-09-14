2f5da9824742f6e8b166716082b46af551312ee0
Joni Lahtinen <joni.lahtinen@gmail.com>
2012-09-14 22:33:41 +0300

    Run test after window is loaded

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-var paper = Raphael(10,50,320,200);
+var test = function() {
+    var paper = Raphael(10,50,320,200);
 
-var circle = paper.circle(50,30,10);
-circle.attr("fill", "#f00");
-circle.attr("stroke", "#fff");
+    var circle = paper.circle(50,30,10);
+    circle.attr("fill", "#f00");
+    circle.attr("stroke", "#fff");
+}
 
+window.onload = test;
